eed2b4f1f040dc61622160fdc8ef3f6e9ca2ae27
kusiimac <cleophaskusiima@gmail.com>
2023-07-26 19:23:43 +0300

    Function that adds, subtracts, multiplies or divides two doubles by entering a specific character.

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,28 @@
+/**
+  Write a function that has three parameters, two doubles and one character (+, -, *, /) and
+  check if the third parameter:
+  == *, it will multiply the two doubles
+  == +, it will add the two numbers
+  == _, it will subtract the two numbers
+  == /, it will divide the two numbers
+ */
+fun main(){
+    print("Enter first number: ")
+    val firstNumber = readln().toDouble()
+    print("Enter second number: ")
+    val secondNumber = readln().toDouble()
+    print("Enter character: ")
+    val character = readln()
+    val answer = arithmeticOperationWithCharacter(firstNumber, secondNumber, character)
+    print(answer)
+}
+fun arithmeticOperationWithCharacter(a:Double, b:Double, c:String):Double {
+    return when (c) {
+        "+" -> { a + b }
+        "-" -> { a - b }
+        "*" -> { a * b }
+        "/" -> { a / b }
+        else -> {
+            println("You entered an invalid character"); 0.0 }
+    }
+}
